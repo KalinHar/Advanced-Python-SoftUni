d566c799b8689ecb4e5b9d232ce9b6bc4cfd9433
KalinHar <kalix@abv.bg>
2021-04-20 10:09:45 +0300

    new

--- tuples and sets/softuni party.py
+++ tuples and sets/softuni party.py
@@ -5,16 +5,22 @@ def entry_list(n):
     return lines
 
 
+def entry_list_while(key_word):
+    result = []
+    while True:
+        line = input()
+        if line == key_word:
+            break
+        result.append(line)
+    return result
+
+
 count = int(input())
-reservations = entry_list(count)
+reservations = set(entry_list(count))
 
-guests = set('')
-command = input()
-while command != 'END':
-    guests.add(command)
-    command = input()
+guests = set(entry_list_while("END"))
 
-un_come_guests = set(reservations).difference(guests)
+un_come_guests = reservations.difference(guests)
 print(len(un_come_guests))
 for g in sorted(un_come_guests):
     print(g)
