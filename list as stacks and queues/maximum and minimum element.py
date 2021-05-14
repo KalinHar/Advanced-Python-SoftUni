14ca6857cb8adc1ac4875fa4b691eaf4b4c6f756
KalinHar <kalix@abv.bg>
2021-05-14 09:23:23 +0300

    new

---
+++ list as stacks and queues/maximum and minimum element.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+sequence = []
+for _ in range(n):
+    query = input().split()
+    if query[0] == "1":
+        sequence.append(int(query[1]))
+    elif query[0] == "2" and sequence:
+        sequence.pop()
+    elif query[0] == "3":
+        print(max(sequence))
+    elif query[0] == "4":
+        print(min(sequence))
+
+reversed_sequence = []
+while sequence:
+    reversed_sequence.append(sequence.pop())
+print(*reversed_sequence, sep=", ")
