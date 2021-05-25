9266125f421bdf819b66a4e2ce53e2c7eb1ac458
KalinHar <kalix@abv.bg>
2021-05-25 20:40:13 +0300

    red

--- tuples and sets/parking lot.py
+++ tuples and sets/parking lot.py
@@ -8,16 +8,15 @@ def entry_list(n):
 count = int(input())
 cars = entry_list(count)
 
-park_cars = set("")
+park_cars = set()
 for car in cars:
     command, number = car.split(", ")
     if command == "IN":
         park_cars.add(number)
     elif command == "OUT":
-        park_cars.remove(number)
+        park_cars.discard(number)  # remove without error if not exist
 
 if park_cars:
-    for c in park_cars:
-        print(c)
+    print('\n'.join(park_cars))
 else:
     print("Parking Lot is Empty")
--- tuples and sets/record unique names.py
+++ tuples and sets/record unique names.py
@@ -1,13 +1,20 @@
-def entry_list(n):
-    lines = []
-    for _ in range(n):
-        lines.append(input())
-    return lines
+# def entry_list(n):
+#     lines = []
+#     for _ in range(n):
+#         lines.append(input())
+#     return lines
+#
+#
+# count = int(input())
+# names = entry_list(count)
+#
+# unique_names = set(names)
+# for n in unique_names:
+#     print(n)
 
-
-count = int(input())
-names = entry_list(count)
-
-unique_names = set(names)
-for n in unique_names:
+n = int(input())
+names = set()
+for _ in range(n):
+    names.add(input())
+for n in names:
     print(n)
