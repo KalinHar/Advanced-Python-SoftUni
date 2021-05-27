a54117f2b058c8a72d335fb685de2ff868200ef1
KalinHar <kalix@abv.bg>
2021-05-27 19:11:12 +0300

    red

--- tuples and sets/set of elements.py
+++ tuples and sets/set of elements.py
@@ -1,11 +1,11 @@
-n, m = (int(x) for x in input().split())
+n, m = input().split()
 
 first_set = set()
 second_set = set()
 
-for _ in range(n):
+for _ in range(int(n)):
     first_set.add(input())
-for _ in range(m):
+for _ in range(int(m)):
     second_set.add(input())
 
 print("\n".join(first_set.intersection(second_set)))
