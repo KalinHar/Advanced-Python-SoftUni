9826a92ba32fbd196781add483c718b84bf93dc8
KalinHar <kalix@abv.bg>
2021-05-27 19:53:12 +0300

    red

--- tuples and sets/count symbols.py
+++ tuples and sets/count symbols.py
@@ -4,6 +4,7 @@ result = {}
 for symbol in text:
     if symbol not in result:
         result[symbol] = text.count(symbol)
+    # result[symbol] = result.get(symbol, 0) + 1
 
 for k, v in sorted(result.items()):
     print(f'{k}: {v} time/s')
