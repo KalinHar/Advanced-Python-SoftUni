5b4b4c6c252c401d7b64f3d91b4935d2984afeb8
KalinHar <kalix@abv.bg>
2021-05-28 20:44:17 +0300

    red

--- multidimension lists/primari diagonal.py
+++ multidimension lists/primari diagonal.py
@@ -1,11 +1,12 @@
 n = int(input())
 matrix = []
+diagonal_sum = 0
 
 for r in range(n):
     matrix.append([int(x) for x in input().split()])
+    diagonal_sum += matrix[r][r]
 
-diagonal_sum = 0
-for i in range(n):
-    diagonal_sum += matrix[i][i]
+# for i in range(n):
+#     diagonal_sum += matrix[i][i]
 
 print(diagonal_sum)
