619a1e4366916b303f9fdb01abd7b08eccd9eeb0
KalinHar <kalix@abv.bg>
2021-05-31 21:13:25 +0300

    red

--- multidimension lists/1-diagonal-difference.py
+++ multidimension lists/1-diagonal-difference.py
@@ -17,3 +17,16 @@ for i in range(n):
     sum_right_up += m_trix[i][n -1 - i]
 
 print(abs(sum_right_down - sum_right_up))
+
+
+# n = int(input())
+# matrix = []
+# diagonal1 = 0
+# diagonal2 = 0
+# for _ in range(n):
+#     row = [int(x) for x in input().split()]
+#     matrix.append(row)
+# for index in range(n):
+#     diagonal1 += matrix[index][index]
+#     diagonal2 += matrix[index][~index]
+# print(f"{abs(diagonal1-diagonal2)}")
