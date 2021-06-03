bf4495fdfe2e21d1cc4a6316f1efb81fe8bfa7ae
KalinHar <kalix@abv.bg>
2021-06-03 19:53:03 +0300

    red

--- comprehension/4-number-clasification.py
+++ comprehension/4-number-clasification.py
@@ -3,3 +3,11 @@ result = {'Positive:': [el for el in nums if el >= 0], 'Negative:': [el for el i
 for k, v in result.items():
     n = ", ".join([str(x) for x in v])
     print(f"{k} {n}")
+
+# positive = []
+# negative = []
+# [negative.append(x) if x < 0 else positive.append(x) for x in nums]
+#
+# odd = []
+# even = []
+# [even.append(x) if x % 2 == 0 else odd.append(x) for x in nums]
--- workshop/testwork.py
+++ workshop/testwork.py
@@ -4,17 +4,29 @@ def num_of_player(t):
     return 2
 
 
-def get_turn(t, free_r,):
+def get_turn(t, free_r):
     player = num_of_player(t)
     while True:
         print(f"Player {player}, please choose a column.")
-        column = int(input()) - 1
-        if column in range(r) and free_r[column] >= 0:
+        column_num = int(input()) - 1
+        if column_num in range(r) and free_r[column_num] >= 0:
+            return free_r[column_num], column_num, player
+        print("Invalid column index!")
 
+
+def push_mark(i, j, num):
+    global game_board
+    game_board[i][j] = num
 
 
 r, c = [int(x) for x in input().split()]
-
 game_board = [[int(x) for x in list("0" * c)] for _ in range(r)]
 free_rows_in_column = [r-1 for _ in range(c)]
 turns = 1
+
+while True:
+    row, col, mark = get_turn(turns, free_rows_in_column)
+    free_rows_in_column[col] -= 1
+    turns += 1
+    push_mark(row, col, mark)
+    [print(row) for row in game_board]
