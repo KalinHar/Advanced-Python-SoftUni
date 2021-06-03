4a68c4a12a2a5848cef3ca82faf6609be8b95c0f
KalinHar <kalix@abv.bg>
2021-06-03 18:38:48 +0300

    red

--- workshop/testwork.py
+++ workshop/testwork.py
@@ -4,14 +4,17 @@ def num_of_player(t):
     return 2
 
 
-def game(t, board):
+def get_turn(t, free_r,):
     player = num_of_player(t)
-    columns = f"Player {player}, please"
+    while True:
+        print(f"Player {player}, please choose a column.")
+        column = int(input()) - 1
+        if column in range(r) and free_r[column] >= 0:
 
 
 
 r, c = [int(x) for x in input().split()]
 
 game_board = [[int(x) for x in list("0" * c)] for _ in range(r)]
-print(game_board)
+free_rows_in_column = [r-1 for _ in range(c)]
 turns = 1
