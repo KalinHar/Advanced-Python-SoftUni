a9cb6014892dfa980ebe95bccfa5f36df565d35b
KalinHar <kalix@abv.bg>
2021-06-04 19:47:35 +0300

    new

---
+++ functions/absolute_values.py
@@ -0,0 +1,9 @@
+# def abs_values(seq):
+#     return [abs(x) for x in seq]
+            # list(map(abs, seg))
+
+print([abs(float(x)) for x in input().split()])
+
+# nums = [float(x) for x in input().split()]
+# nums_gen = map(float, input().split())
+# nums_gen = map(lambda x: float(x), input().split())
---
+++ functions/roinding.py
@@ -0,0 +1,4 @@
+print([round(float(x)) for x in input().split()])
+
+# nums = [float(x) for x in input().split()]
+# r_nums = list(map(round, nums))
