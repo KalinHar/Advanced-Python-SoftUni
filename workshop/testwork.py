d0cc1a06a28746e06a6b6f901736377d653c42d1
KalinHar <kalix@abv.bg>
2021-06-06 19:45:49 +0300

    new

--- workshop/testwork.py
+++ workshop/testwork.py
@@ -1,16 +1,16 @@
-def num_of_player(t):
-    if t % 2:
+def num_of_player(turn):
+    if turn % 2:
         return 1
     return 2
 
 
-def get_turn(t, free_r):
-    player = num_of_player(t)
+def get_turn(turn, free_r_in_c):
+    player = num_of_player(turn)
     while True:
         print(f"Player {player}, please choose a column.")
-        column_num = int(input()) - 1
-        if column_num in range(r) and free_r[column_num] >= 0:
-            return free_r[column_num], column_num, player
+        number_of_column = int(input()) - 1
+        if number_of_column in range(rows) and free_r_in_c[number_of_column] >= 0:
+            return free_r_in_c[number_of_column], number_of_column, player
         print("Invalid column index!")
 
 
@@ -23,21 +23,109 @@ def print_board():
     [print(row) for row in game_board]
 
 
-def check_for_winner():
-    pass
+def check_for_winner(i, j, num):
+    winning_lines = [
+        [[[+1, -1], [+2, -2], [+3, -3]], [[-1, +1], [-2, +2], [-3, +3]]],  # diagonal: left,down - right,up
+        [[[+1, +1], [+2, +2], [+3, +3]], [[-1, -1], [-2, -2], [-3, -3]]],  # diagonal: right,down - left,up
+        [[[0, -1], [0, -2], [0, -3]], [[0, +1], [0, +2], [0, +3]]],        # horizontal: left - right
+        [[[+1, 0], [+2, 0], [+3, 0]], [[-1, 0], [-2, 0], [-3, 0]]],        # vertical: down - !(up)
+    ]
+    for line in winning_lines:
+        four_in_line = 1
+        for half_line in line:
+            for row_col in half_line:
+                n_row, n_col = i + row_col[0], j + row_col[1]
+                if n_row in range(rows) and n_col in range(columns) and game_board[n_row][n_col] == num:
+                    four_in_line += 1
+                    if four_in_line == 4:
+                        return num
+                else:
+                    break
 
 
-r, c = [int(x) for x in input().split()]
-game_board = [[0 for col in range(c)] for row in range(r)]
-free_rows_in_column = [r-1 for _ in range(c)]
+# def check_for_winner(i, j, num):
+#     four_in_line = 1
+#     # check left-down and right-up diagonal
+#     for n in range(1, 4):
+#         if i + n in range(r) and j - n in range(c) and game_board[i + n][j - n] == num:
+#             four_in_line += 1
+#             if four_in_line == 4:
+#                 return num
+#         else:
+#             break
+#     for n in range(1, 4):
+#         if i - n in range(r) and j + n in range(c) and game_board[i - n][j + n] == num:
+#             four_in_line += 1
+#             if four_in_line == 4:
+#                 return num
+#         else:
+#             four_in_line = 1
+#             break
+#     # check right-down and left-up diagonal
+#     for n in range(1, 4):
+#         if i + n in range(r) and j + n in range(c) and game_board[i + n][j + n] == num:
+#             four_in_line += 1
+#             if four_in_line == 4:
+#                 return num
+#         else:
+#             break
+#     for n in range(1, 4):
+#         if i - n in range(r) and j - n in range(c) and game_board[i - n][j - n] == num:
+#             four_in_line += 1
+#             if four_in_line == 4:
+#                 return num
+#         else:
+#             four_in_line = 1
+#             break
+#     # check horizontal
+#     for n in range(1, 4):
+#         if i in range(r) and j - n in range(c) and game_board[i][j - n] == num:
+#             four_in_line += 1
+#             if four_in_line == 4:
+#                 return num
+#         else:
+#             break
+#     for n in range(1, 4):
+#         if i in range(r) and j + n in range(c) and game_board[i][j + n] == num:
+#             four_in_line += 1
+#             if four_in_line == 4:
+#                 return num
+#         else:
+#             four_in_line = 1
+#             break
+#     # check vertical
+#     for n in range(1, 4):
+#         if i + n in range(r) and j in range(c) and game_board[i + n][j] == num:
+#             four_in_line += 1
+#             if four_in_line == 4:
+#                 return num
+#         else:
+#             break
+#     for n in range(1, 4):
+#         if i - n in range(r) and j in range(c) and game_board[i - n][j] == num:
+#             four_in_line += 1
+#             if four_in_line == 4:
+#                 return num
+#         else:
+#             four_in_line = 1
+#             break
+
+
+rows, columns = [int(x) for x in input().split()]
+game_board = [[0 for col in range(columns)] for row in range(rows)]
+free_row_in_columns = [rows - 1 for _ in range(columns)]
 turns = 1
 
 while True:
-    row, col, mark = get_turn(turns, free_rows_in_column)
-    free_rows_in_column[col] -= 1
+    row, col, mark = get_turn(turns, free_row_in_columns)
+    free_row_in_columns[col] -= 1
     push_mark(row, col, mark)
     print_board()
-    if turns == r * c:
+    winner = check_for_winner(row, col, mark)
+    if winner:
+        print(f"The Winner is Player {winner}!")
+        break
+    if turns == rows * columns:
         print("No winner!")
         print_board()
         break
