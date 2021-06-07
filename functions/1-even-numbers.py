b5a08b4fba20bd8c927a6ae817a3b9161c29e592
KalinHar <kalix@abv.bg>
2021-06-07 21:33:30 +0300

    new

--- functions/1-even-numbers.py
+++ functions/1-even-numbers.py
@@ -1,1 +1,8 @@
 print(list(filter(lambda x: x % 2 == 0, map(int, input().split()))))
+
+
+def is_even(num):
+    return num % 2 == 0
+
+
+even_nums = list(filter(is_even, [1,2,3,4,5,6]))
--- functions/4-negative-positive.py
+++ functions/4-negative-positive.py
@@ -10,3 +10,13 @@ if abs(sum(negative)) > sum(positive):
     print("The negatives are stronger than the positives")
 else:
     print("The positives are stronger than the negatives")
+
+
+# def is_not_negative(num):
+#     return num >= 0
+#
+#
+# nums = list(map(int, input().split()))
+# positive_1 = list(filter(is_not_negative, nums))
+# positive = list(filter(lambda x: x == is_not_negative, nums))
+# negative = list(filter(lambda x: x != is_not_negative, nums))
