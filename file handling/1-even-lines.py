0b3ea60c05018b20d3705a230199a7de482b66f4
KalinHar <kalix@abv.bg>
2021-06-10 18:41:26 +0300

    new

--- file handling/1-even-lines.py
+++ file handling/1-even-lines.py
@@ -4,11 +4,12 @@ def get_file_content(path):
 
 
 path = '../files/text.txt'
+set_of_chars = {'.', ',', '-', '!', '?'}
 
 lines = (get_file_content(path))
 for i in range(0, len(lines), 2):
     line = lines[i]
-    for char in "-,.!?":
+    for char in set_of_chars:
         line = line.replace(char, "@")
     print(" ".join(reversed(line.split())))
 
