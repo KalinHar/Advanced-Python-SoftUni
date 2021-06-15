3e51c9d92374cdb3b203a824ccb1288d81f5be9a
KalinHar <kalix@abv.bg>
2021-06-15 19:24:44 +0300

    new

--- exams/april-02.py
+++ exams/april-02.py
@@ -5,7 +5,7 @@ def check_player(counter):
 
 
 def sum_of_4(r, c):
-    return sum(map(int, (board[r][0], board[r][6], board[0][c], board[6][c])))
+    return sum(map(int, (board[r][0], board[r][-1], board[0][c], board[-1][c])))
 
 
 first_player, second_player = input().split(', ')
@@ -14,11 +14,10 @@ board = [[x for x in input().split(" ")] for _ in range(7)]
 count = 0
 
 while True:
-    row, col = eval(input())
+    row, col = [int(x) for x in input()[1:-1].split(', ')]  # row, col = eval(input())
     count += 1
     if row not in range(7) or col not in range(7):
         continue
-
     if board[row][col] == "B":
         break
     elif board[row][col] == "T":
@@ -32,10 +31,4 @@ while True:
     if players[check_player(count)] <= 0:
         break
 
-winner = check_player(count)
-if winner == first_player:
-    count_turns = (count + 1) / 2
-else:
-    count_turns = count / 2
-
-print(f"{winner} won the game with {int(count_turns)} throws!")
+print(f"{check_player(count)} won the game with {(count + 1) // 2} throws!")
