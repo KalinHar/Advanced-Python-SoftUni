4980789e9d8abc38c84ab6c5c5a6d80ec20f2b54
KalinHar <kalix@abv.bg>
2021-06-18 12:09:46 +0300

    new

--- workshop/four_in_line/main.py
+++ workshop/four_in_line/main.py
@@ -6,16 +6,17 @@ game_board = [[0 for col in range(columns)] for row in range(rows)]
 free_row_in_columns = [rows - 1 for _ in range(columns)]
 turns = 1
 
-while True:
-    row, col, mark = get_turn(turns, free_row_in_columns, rows)
-    free_row_in_columns[col] -= 1
-    push_mark(row, col, mark, game_board)
-    [print(row) for row in game_board]
-    winner = check_for_winner(row, col, mark, rows, columns, game_board)
-    if winner:
-        print(f"The Winner is Player {winner}!")
-        break
-    if turns == rows * columns:
-        print("No winner!")
-        break
-    turns += 1
+if __name__ == "__main__":
+    while True:
+        row, col, mark = get_turn(turns, free_row_in_columns, rows)
+        free_row_in_columns[col] -= 1
+        push_mark(row, col, mark, game_board)
+        [print(row) for row in game_board]
+        winner = check_for_winner(row, col, mark, rows, columns, game_board)
+        if winner:
+            print(f"The Winner is Player {winner}!")
+            break
+        if turns == rows * columns:
+            print("No winner!")
+            break
+        turns += 1
